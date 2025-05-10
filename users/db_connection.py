50b875cad11900f0a830ef5c69ad7a431b9ab5ee
ankit-pal-rgb <ankitp@heaptrace.com>
2025-05-10 10:43:39 +0530

    Latest

--- users/db_connection.py
+++ users/db_connection.py
@@ -2,4 +2,11 @@ from pymongo import MongoClient
 import os
 client = MongoClient("mongodb://localhost:27017/")
 db = client["mydb"]
+
+if "Users" not in db.list_collection_names():
+    db.create_collection("Users")
+    print("Users collection created.")
+else:
+    print("Users collection already exists.")
+    
 users_collection=db["Users"]
